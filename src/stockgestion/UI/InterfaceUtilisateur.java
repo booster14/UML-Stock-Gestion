e584f45d268dd651dda03850f52277c53a8c2784
rubeus90 <rubeus90@gmail.com>
2014-12-28 17:06:01 +0100

    Singleton pour classe UI et supprimer main()

--- src/stockgestion/UI/InterfaceUtilisateur.java
+++ src/stockgestion/UI/InterfaceUtilisateur.java
@@ -10,12 +10,17 @@ package stockgestion.UI;
  * @author rubeus
  */
 public class InterfaceUtilisateur extends javax.swing.JFrame {
+    private static InterfaceUtilisateur instance = null;
 
-    /**
-     * Creates new form InterfaceUtilisateur
-     */
-    public InterfaceUtilisateur() {
+    private InterfaceUtilisateur() {
         initComponents();
+    }
+    
+    public static InterfaceUtilisateur getInstance(){
+        if(instance == null){
+            instance = new InterfaceUtilisateur();
+        }
+        return instance;
     }
 
     /**
@@ -43,40 +48,6 @@ public class InterfaceUtilisateur extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InterfaceUtilisateur.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InterfaceUtilisateur.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InterfaceUtilisateur.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InterfaceUtilisateur.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new InterfaceUtilisateur().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
