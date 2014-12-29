48755ecf01d3bf9bd1e8c15635cf040f83a37550
7h1b0 <wyrm1992@hotmail.fr>
2014-12-29 18:41:57 +0100

    Ajout des getters et setters pour la classe Fournisseur et Commande

--- src/stockgestion/Entite/Commande.java
+++ src/stockgestion/Entite/Commande.java
@@ -2,8 +2,32 @@ package stockgestion.Entite;
 
 public class Commande {
 
-	private Article article;
-	private int quantite;
-	private int id;
+    private Article article;
+    private int quantite;
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    private int id;
 
 }
--- src/stockgestion/Entite/Fournisseur.java
+++ src/stockgestion/Entite/Fournisseur.java
@@ -1,11 +1,50 @@
 package stockgestion.Entite;
 
 public class Fournisseur {
+    private String nom;
+    private int id;
+    private String addresse;
+    private int codePostal;
+    private int numeroTelephone;
 
-	private String nom;
-	private int id;
-	private String addresse;
-	private int codePostal;
-	private int numeroTelephone;
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public int getNumeroTelephone() {
+        return numeroTelephone;
+    }
+
+    public void setNumeroTelephone(int numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
 
 }
