166334a92d78f29791177a57b04c0bb6995be8eb
rubeus90 <rubeus90@gmail.com>
2015-01-02 17:25:45 +0100

    Ajouter une classse Verificateur pour verifier les champs d'entree utilisateur

---
+++ src/stockgestion/Outil/Verificateur.java
@@ -0,0 +1,20 @@
+package stockgestion.Outil;
+
+public class Verificateur {
+    
+    //Il ne faut pas de caracteres speciales qui fait merder les requetes sql, comme le ', ", etc
+    //Et faut que string non vide
+    public static boolean isValidString(String texte){
+        return false;
+    }    
+    
+    //String non vide puis je sais pas....
+    public static boolean isValidInt(String texte){
+        return false;
+    }
+    
+    //String non vide etc
+    public static boolean isValidDouble(String texte){
+        return false;
+    }
+}
