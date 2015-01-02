88da8e181af9cdf26e8980f1a058a3a71a28a155
rubeus90 <rubeus90@gmail.com>
2015-01-02 18:37:14 +0100

    Ajouter methode pour verifier la validite de la selection dans la liste des fournisseurs

--- src/stockgestion/Outil/Verificateur.java
+++ src/stockgestion/Outil/Verificateur.java
@@ -2,23 +2,28 @@ package stockgestion.Outil;
 
 import java.awt.Color;
 import javax.swing.JComponent;
+import javax.swing.JList;
 
 public class Verificateur {
     
     //Il ne faut pas de caracteres speciales qui fait merder les requetes sql, comme le ', ", etc
     //Et faut que string non vide
     public static boolean isValidString(String texte){
-        return false;
+        return true;
     }    
     
     //String non vide puis je sais pas....
     public static boolean isValidInt(String texte){
-        return false;
+        return true;
     }
     
     //String non vide etc
     public static boolean isValidDouble(String texte){
-        return false;
+        return true;
+    }
+    
+    public static boolean isValidSelection(JList list){
+        return list.getSelectedIndices().length > 0;
     }
     
     //Mettre le champs non valide en rouge
